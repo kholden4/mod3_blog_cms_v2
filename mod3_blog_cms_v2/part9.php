245070ce08c8819c56620fae0d037aa9ab4ae50d
kholden4 <kholden4@lesley.edu>
2019-10-05 12:23:47 -0700

    redo

--- mod3_blog_cms_v2/part9.php
+++ mod3_blog_cms_v2/part9.php
@@ -2,29 +2,9 @@
     ini_set('display_errors', 1); // Show errors for debugging
 ?>
 
-// VariablesExample 
-<?php
-    $firstName = "Jane";                     // String type (text characters)
-    $lastName = "Doe";                       // String type (text characters)
-    $age = 35;                               // Integer type (whole numbers)
-    $married = true;                         // Boolean type (True/False)
-    $childrenNames = array("Alice", "Bob");  // Array type (use of values)
-?>
-
-
-// FunctionExample
- <?php
-    function getFullName($firstName, $lastName) {     // Function name
-        return  $firstName . " " . $lastName;         // Concatenation Operator & Returned value
-    }
-
-    // To call or use the above function
-    $fullName = getFullName($firstName, $lastName);   // Using Variables
-    $fullName = getFullName("Alice", "Doe");          // Using Values
-?>
  
 <?php
-// New Function Example
+// Function Example
     function getIsSettled($age, $married) {          // Function Name
         $dogYears = $age/7;                          // Arithmetic Operator
         $isAdult = $dogYears > 4;                    // Comparison operator
@@ -34,8 +14,7 @@
     
 // $isSettled = getIsSettled($age, $married);       // Using variables
 // $isSettled = getIsSettled(5, $false);            // Using Values
-?>
-// EchoExample   
+?>  
 
 <?php
     if ($isSettled) {                               // Conditional Statements
