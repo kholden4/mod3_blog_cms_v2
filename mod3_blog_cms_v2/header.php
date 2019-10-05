ffe92ff52ce095a3fc4bb3946e0448ec97f11903
kholden4 <kholden4@lesley.edu>
2019-10-05 12:06:44 -0700

    added part9.php link to header

--- mod3_blog_cms_v2/header.php
+++ mod3_blog_cms_v2/header.php
@@ -11,6 +11,7 @@
           <ul>
             <li><a href="index.php">HOMEPAGE</a></li>
             <li><a href="post-submission.php">CREATE NEW POST</a></li>
+            <li><a href="part9.php">PART NINE</a></li>
           </ul>
         </nav><!--close for navigation-->    
     </header><!--close for header--> 
