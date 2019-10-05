61bd18dc01efe759a007c28012211532d7b64d3d
kholden4 <kholden4@lesley.edu>
2019-10-05 13:50:53 -0700

    Update part 10

--- mod3_blog_cms_v2/index.php
+++ mod3_blog_cms_v2/index.php
@@ -6,10 +6,24 @@
     <body>
 
         <main>
+            <?php   
+                function getPostTitlesFromDatabase () {
+                    // TODO in Modual 4
+                    // get this data from a database instead of hardcoding it
+                    $postTitles - array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+                    return $postTitles;
+                }
+            ?>
             <h3>Previous Posts</h3>
-            <ul>
-                <li><a href="post.php">My First Entry</a></li>
-            </ul>
+                <ul>
+                    <?php
+                        $postTitles = getPostTitlesFromDatabase();
+
+                        foreach($postTitles as $postTitle) {
+                            echo "<li><a href='post.php'>" . $postTitle . "</a></li>";
+                        }
+                     ?>   
+                </ul>
         </main><!--close for main-->
 
     </body><!--close for body-->   
