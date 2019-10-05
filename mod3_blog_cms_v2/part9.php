b67603272e704df57c18a2b9eca4ee472d185845
kholden4 <kholden4@lesley.edu>
2019-10-05 12:10:13 -0700

    added php coding to each section

--- mod3_blog_cms_v2/part9.php
+++ mod3_blog_cms_v2/part9.php
@@ -9,7 +9,8 @@
     $age = 35;                               // Integer type (whole numbers)
     $married = true;                         // Boolean type (True/False)
     $childrenNames = array("Alice", "Bob");  // Array type (use of values)
-    
+?>
+
  //<!-- Part 9.3 Operators --> 
         //Arithmetic Operators: + - *   
         //Concatenation Operators: joins two stings together ex. "Hello" and "World"
@@ -17,7 +18,7 @@
         //Comparison Operators: compares two values or variables, always result in a boolean (T/F). To see equal to use ==. To see less than use <.
 
  //<!-- Part 9.4 Functions -->  
-
+ <?php
     function getFullName($firstName, $lastName) {     // Function name
         return  $firstName . " " . $lastName;         // Concatenation Operator & Returned value
     }
@@ -25,8 +26,9 @@
     //To call/use the above function
     $fullName = getFullName($firstName, $lastName);   // Using Variables
     $fullName = getFullName("Alice", "Doe");          // Using Values
-
-    
+?>
+ 
+<?php
 //New Function Example:
     function getIsSettled($age, $married) {          // Function Name
         $dogYears = $age/7;                          // Arithmetic Operator
@@ -37,18 +39,19 @@
     
     $isSettled = getIsSettled($age, $married);       // Using variables
     $isSettled = getIsSettled(5, $false);            // Using Values
+?>
 
 //!<-- Part 9.5 Echo --> 
 
 echo $fullName
 echo "<h1>Hello Worls!</h1>"                        // String example
 
+<?php
     if ($isSettled) {                               // 9.6 Conditional Statements
         echo $fullName . " is settled.";
     } else {
         echo $fullName . " is not settled.";
     }
-
 
     foreach($childrenNames as $childName) {         // 9.7 Loops
         echo "<li>" . $childName . "</li>";
