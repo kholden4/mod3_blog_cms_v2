6532841727697d917fdd87e9853dc3f4920987df
kholden4 <kholden4@lesley.edu>
2019-10-05 10:29:26 -0700

    updated files names to php extentions

---
+++ mod3_blog_cms_v2/post.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+    <head>
+        <title>Kristy's Portfolio & Blog | 19.10</title>
+        <nav>
+          <ul>
+            <li><a href="index.php">HOMEPAGE</a></li>
+            <li><a href="post-submission.php">CREATE NEW POST</a></li>
+          </ul>
+        </nav><!--close for navigation-->
+    </head><!--close for head-->
+
+    <body>
+        <main>
+
+            <div id="header">
+                <h1>Kristy's Portfolio & Blog</h1>
+                <h2>Data Coordinator by Day | UX Design Student by Night</h2>
+            </div><!--close for header-->
+
+            <div id="post-title"> 
+                <h3>My First Entry</h3>
+            </div><!--close for post-title-->
+
+            <div id="post-author">
+                <h4>Kristy Holden</h4>
+            </div><!--close for post-author-->
+
+            <div id="publish-date">
+                <h4>10.4.19</h4>
+            </div><!--close for publish-date-->
+
+            <div id="post-content">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div><!--close for post-content-->
+
+        </main><!--close for main-->
+
+        <footer>
+            <p><strong>Created by:</strong> Kristy Holden - San Diego, CA</p>
+            <p><strong>Contact information:</strong> <a href="mailto:kholden4@lesley.edu">Kholden4@lesley.edu</a></p>
+        </footer><!--close for footer-->
+    </body><!--close for body-->
+    
+</html>
